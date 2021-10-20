b216e3593ef7b2fa33eccc304fe6675e7b4c3517
Alexander Vladimirov <alexander.vladimirov@s-telematics.ru>
2021-10-20 17:28:15 +0300

    Fix age, Add moment-with-locales.min.js.map

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     let now = moment();
     let stDate = moment('20201008');
     let eoDate = moment('20210709');
-    let expDate = moment('20190501');
+    let expDate = moment('20190909');
+    let age = moment('19890902');
 
+    document.getElementById('age').append(moment.duration(now.diff(age, 'years'), 'years').humanize());
     document.getElementById('exp').append(moment.duration(now.diff(expDate)).humanize());
     document.getElementById('eo').append(moment.duration(now.diff(eoDate, 'month') + 1, 'month').humanize());
     document.getElementById('st').append(moment.duration(eoDate.diff(stDate, 'month') + 1, 'month').humanize());
