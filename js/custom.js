12afa0c0fb7c6a6370a9c387a450a904e07b13bf
Alexander Vladimirov <alexander@vladimirov.msk.ru>
2022-03-27 01:16:33 +0300

    Update experience

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,20 @@
+function appendExperience(id, endDate, startDate) {
+    document.getElementById(id).append(moment.duration(endDate.diff(startDate, 'month') + 1, 'month').humanize());
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     moment.locale('ru');
     let now = moment();
     let stDate = moment('20201008');
     let eoDate = moment('20210709');
+    let sftDate = moment('20220328');
     let expDate = moment('20190909');
     let age = moment('19890902');
 
     document.getElementById('age').append(moment.duration(now.diff(age, 'years'), 'years').humanize());
     document.getElementById('exp').append(moment.duration(now.diff(expDate)).humanize());
-    document.getElementById('eo').append(moment.duration(now.diff(eoDate, 'month') + 1, 'month').humanize());
-    document.getElementById('st').append(moment.duration(eoDate.diff(stDate, 'month') + 1, 'month').humanize());
+
+    appendExperience('st', eoDate, stDate);
+    appendExperience('eo', sftDate, eoDate);
+    appendExperience('sft', now, sftDate);
 }, false);
